c80f759d94140e348d0bc815d2d79829f7042f0d
Murray Henwood <murrayhenwood@gmail.com>
2020-03-02 14:03:33 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,6 +13,13 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend "azurerm" {
+    storage_account_name = "terraform_storage_account_name_1234"
+    container_name       = "terraform_container"
+  }
+}
+
 module "az_resource_group" {
   source = "./az_resource_group"
 }
